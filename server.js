f7efba5a20bf56233d0d4114ba609af6f641c6c4
Sebghat Yusuf <sebghatyusuf@gmail.com>
2021-03-16 22:03:49 +0430

    building server - 2021/3/16, 22:3

--- server.js
+++ server.js
@@ -12,9 +12,29 @@ const rooms = {
 
 const io = require('socket.io')(
     require('http').createServer(
-        function(){
+        function () {
 
         }
     ).listen(80)
 )
 
+
+io.on('connection', io => {
+    console.log('Connection established with a client!');
+    // More stuff (coming soon to a chapter near you!)
+});
+
+io.on('validate', (inData, inCallback) => {
+    const user = users[inData.username];
+    if (user) {
+        if (user.password === inData.password) {
+            inCallback({ "status": "ok" })
+        } else {
+            inCallback({ 'status': "fail" })
+        }
+    } else {
+        users[inData.username] = inData;
+        io.emit('newUser', users);
+        inCallback({ 'status': 'created' });
+    }
+})
