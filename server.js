977360daff910308755715408ade15fe3656d9ce
Sebghat Yusuf <sebghatyusuf@gmail.com>
2021-03-16 21:53:48 +0430

    building server - 2021/3/16, 21:53

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const users = {};
+const users = {
+    username: { username: "", password: "" }
+};
+
+
+const rooms = {
+    roomName: "", description: "", maxPeople: 100, private: true | false, creator: "", users: [
+
+    ]
+};
